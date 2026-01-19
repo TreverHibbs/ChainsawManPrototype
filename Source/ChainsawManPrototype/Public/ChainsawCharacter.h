304b2ccb243dc9cd65c876288d4071b9804f65bb
Trever Hibbs <treverhibbs@gmail.com>
2026-01-19 10:57:49 -0800

    add character.h upate

--- Source/ChainsawManPrototype/Public/ChainsawCharacter.h
+++ Source/ChainsawManPrototype/Public/ChainsawCharacter.h
@@ -30,6 +30,10 @@ public:
 	float FallingGravMultiplier; //units are cm
 	//TODO group my favorite movement properties by creating custom properties that
 	//act as proxies for the actual values they edit
+	//TODO wind down the game design exploration. Take some time to reflect on it
+	//TODO before start of next exploration perhaps explore how to disable most UE5 graphics
+	//features so that I am forced to be intentional about the look of the prototype.
+	//Don't use art that will skew how I think the finished game might look to much.
 	
 	UFUNCTION()
 	void Move(const FInputActionValue& ActionValue);
